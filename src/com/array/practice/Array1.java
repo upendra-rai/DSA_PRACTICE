c97432c221b2966825fc0293b91bfe26b8ec9133
Upendra Rai <upendrarai02@gmail.com>
2023-11-19 03:20:58 +0530

    Leet Code Two number Some Problem Solved.

--- src/com/array/practice/Array1.java
+++ src/com/array/practice/Array1.java
@@ -1,8 +1,27 @@
 package com.array.practice;
 
+//Two Sum Question From LeetCode
+
 public class Array1 {
 	public static void main(String[] args) {
 
+		int[] numbers = { 3, 4, 2, 7, 11, 15 };
+		int[] result = twoSum(numbers, 9);
+		for (int data : result) {
+			System.out.println(data);
+		}
+
 	}
 
+	public static int[] twoSum(int[] nums, int target) {
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+					return new int[] { i, j };
+				}
+			}
+		}
+		return nums;
+
+	}
 }
